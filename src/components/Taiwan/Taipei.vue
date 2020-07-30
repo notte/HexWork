33f0c5ba81dd1f7da92af14615a1e237b0c462af
Yoyo <maonome@gmail.com>
2020-07-30 17:59:27 +0800

    切版商品詳細頁ing

--- src/components/Taiwan/Taipei.vue
+++ src/components/Taiwan/Taipei.vue
@@ -1,8 +1,31 @@
 <template>
 	<div class="stroke">
 		<div class="title">
-			<img src alt />
 			<h1>【大人囝仔】森林聚落／紫斑蝶季．霧中仙境</h1>
+			<div class="image">
+				<img src="~@/assets/taiwan/south/kaohsiung-2728058.jpg" alt />
+			</div>
+			<h3>所有行程</h3>
+			<div class="other-time">
+				<el-table :data="tableData" :show-header="false">
+					<el-table-column prop="date"></el-table-column>
+					<el-table-column prop="name"></el-table-column>
+					<el-table-column prop="number"></el-table-column>
+					<el-table-column prop="address"></el-table-column>
+					<el-table-column prop="buy">
+						<el-button>報名</el-button>
+					</el-table-column>
+				</el-table>
+			</div>
+		</div>
+		<div class="content">
+			<h3 class="title">
+				花蓮，不再是後山，是片淨土。
+				<br />有多久沒有好好深呼吸了呢？
+				<br />來花蓮享受好山 好水，大口呼吸！
+				<br />看看山、看看水、看看天空、看看海、看看風吹草動
+				<br />來花蓮走走吧❤️
+			</h3>
 		</div>
 	</div>
 </template>
@@ -14,5 +37,29 @@ import { Component } from 'vue-property-decorator';
 @Component({
 	components: {},
 })
-export default class Taipei extends Vue {}
+export default class Taipei extends Vue {
+	// 錯誤訊息：This relative module was not found
+	// 解法，路徑修改為 -> ~@/assets/taiwan/south/kaohsiung-2728058.jpg
+	tableData: object[] = [
+		{
+			date: '出發日期',
+			name: '星期幾',
+			number: '可賣：1',
+			address: '$10,000',
+		},
+		{
+			date: '出發日期',
+			name: '星期幾',
+			number: '可賣：1',
+			address: '$10,000',
+		},
+		{
+			date: '出發日期',
+			name: '星期幾',
+			number: '可賣：1',
+			address: '$10,000',
+		},
+	];
+}
 </script>
+
