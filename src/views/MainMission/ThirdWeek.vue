5328e09799d9da8f73ee667cf190bdc7ba06c259
Yoyo <maonome@gmail.com>
2020-08-04 17:12:35 +0800

    week3 完成

--- src/views/MainMission/ThirdWeek.vue
+++ src/views/MainMission/ThirdWeek.vue
@@ -1,62 +1,80 @@
 <template>
 	<div class="ThirdWeek">
 		<el-button class="addButton" @click="dialogVisible = true">新增產品</el-button>
-		<el-table :data="tableData">
-			<el-table-column label="分類" prop="category" />
-			<el-table-column label="產品名稱" prop="title" />
-			<el-table-column label="原價" prop="origin_price" />
-			<el-table-column label="售價" prop="price" />
+		<el-table :data="tableData" empty-text="無商品">
+			<el-table-column label="分類">
+				<template slot-scope="scope">
+					<span>{{ scope.row.category }}</span>
+				</template>
+			</el-table-column>
+
+			<el-table-column label="產品名稱">
+				<template slot-scope="scope">
+					<span>{{ scope.row.title }}</span>
+				</template>
+			</el-table-column>
+			<el-table-column label="原價">
+				<template slot-scope="scope">
+					<span>{{ scope.row.origin_price }}</span>
+				</template>
+			</el-table-column>
+			<el-table-column label="售價">
+				<template slot-scope="scope">
+					<span>{{ scope.row.price }}</span>
+				</template>
+			</el-table-column>
 			<el-table-column label="是否啟用">
 				<template slot-scope="scope">
-					<span>{{ scope.row.enabled }}</span>
+					<span :class="{'isTrue':scope.row.enabled}">{{ textType(scope.row.enabled) }}</span>
 				</template>
 			</el-table-column>
 			<el-table-column label="編輯">
 				<template slot-scope="scope">
 					<el-button size="mini" @click="edit(scope.$index)">編輯</el-button>
-					<el-button size="mini" type="danger" @click="handleDelete(scope.$index, scope.row)">刪除</el-button>
+					<el-button size="mini" type="danger" @click="dele(scope.$index)">刪除</el-button>
 				</template>
 			</el-table-column>
 		</el-table>
 
 		<!-- dialog -->
-		<el-dialog :title="form.title==''? '新增產品':'編輯產品'" :visible.sync="dialogVisible" width="30%">
+		<el-dialog :visible.sync="dialogVisible" width="50%" @close="handleClose">
 			<div class="fromImage">
 				<img :src="form.imageUrl" alt />
 			</div>
 			<el-form ref="form" :model="form" label-width="80px">
 				<el-form-item label="圖片網址">
-					<el-input v-model="form.imageUrl"></el-input>
+					<input v-model.lazy="form.imageUrl" />
 				</el-form-item>
 				<el-form-item label="分類">
-					<el-input v-model="form.category"></el-input>
+					<input v-model.lazy="form.category" />
 				</el-form-item>
 				<el-form-item label="單位">
-					<el-input v-model="form.unit"></el-input>
+					<input v-model.lazy="form.unit" />
 				</el-form-item>
 				<el-form-item label="產品名稱">
-					<el-input v-model="form.title" />
+					<input v-model.lazy="form.title" />
 				</el-form-item>
 				<el-form-item label="原價">
-					<el-input type="number" v-model="form.origin_price" />
+					<input v-model.number.lazy="form.origin_price" />
 				</el-form-item>
 				<el-form-item label="售價">
-					<el-input type="number" v-model="form.price" />
+					<input v-model.number.lazy="form.price" />
 				</el-form-item>
 				<el-form-item label="是否啟用">
-					<el-switch v-model="form.enabled"></el-switch>
+					<el-switch v-model="form.enabled" active-text="啟用" inactive-text="不啟用"></el-switch>
 				</el-form-item>
 				<el-form-item label="產品描述">
-					<el-input type="textarea" v-model="form.content"></el-input>
+					<textarea type="textarea" v-model.lazy="form.content" />
 				</el-form-item>
 				<el-form-item label="說明內容">
-					<el-input type="textarea" v-model="form.description"></el-input>
+					<textarea type="textarea" v-model.lazy="form.description" />
 				</el-form-item>
 			</el-form>
 
 			<span slot="footer" class="dialog-footer">
 				<el-button @click="dialogVisible = false">取消</el-button>
-				<el-button type="primary" @click="add(form)">確定</el-button>
+				<el-button type="primary" v-if="addHide" @click="add(form)">確定</el-button>
+				<el-button type="primary" v-if="hide" @click="modify(form)">修改</el-button>
 			</span>
 		</el-dialog>
 	</div>
@@ -69,6 +87,8 @@ import * as Modal from '@/models/interfaces/common';
 
 @Component
 export default class ThirdWeek extends Vue {
+	hide: boolean = false;
+	addHide: boolean = true;
 	dialogVisible: boolean = false;
 	tableData: Modal.ThirdWeek[] = [
 		{
@@ -94,33 +114,59 @@ export default class ThirdWeek extends Vue {
 		price: 0,
 		unit: '',
 	};
-	initialForm: Modal.ThirdWeek = {
-		title: '',
-		category: '',
-		content: '',
-		description: '',
-		imageUrl: '',
-		enabled: false,
-		origin_price: 0,
-		price: 0,
-		unit: '',
-	};
 
 	add(form: Modal.ThirdWeek) {
-		for (let item in this.tableData) {
-			if (this.tableData[item].title !== form.title) {
-				this.tableData.push(form);
-			} else {
+		this.tableData.push(form);
+		this.dialogVisible = false;
+	}
+
+	modify(form: Modal.ThirdWeek) {
+		for (const item in this.tableData) {
+			if (this.tableData[item].title === form.title) {
 				this.tableData[item] = form;
 			}
 		}
-		this.form = this.initialForm;
 		this.dialogVisible = false;
 	}
 
 	edit(index: number) {
 		this.dialogVisible = true;
+		this.hide = true;
+		this.addHide = false;
 		this.form = this.tableData[index];
+	}
+
+	handleClose() {
+		this.hide = false;
+		this.addHide = true;
+		this.form = {
+			title: '',
+			category: '',
+			content: '',
+			description: '',
+			imageUrl: '',
+			enabled: false,
+			origin_price: 0,
+			price: 0,
+			unit: '',
+		};
+		this.dialogVisible = false;
+	}
+
+	dele(index: number) {
+		this.$confirm('確認刪除？')
+			.then((_) => {
+				this.tableData.splice(index, 1);
+			})
+			.catch((_) => {});
+	}
+
+	textType(text: boolean) {
+		if (text === false) {
+			return '未啟用';
+		} else {
+			return '啟用';
+		}
 	}
 }
 </script>
@@ -138,5 +184,43 @@ export default class ThirdWeek extends Vue {
 		width: 100%;
 		position: relative;
 	}
+}
+
+.el-dialog__body {
+	.el-form-item {
+		width: 45%;
+		display: inline-block;
+	}
+	.el-form-item:nth-child(8),
+	.el-form-item:nth-child(9) {
+		width: 90%;
+		textarea {
+			width: 100%;
+		}
+	}
+}
+input,
+textarea {
+	-webkit-appearance: none;
+	background-color: #fff;
+	background-image: none;
+	border-radius: 4px;
+	border: 1px solid #dcdfe6;
+	box-sizing: border-box;
+	color: #606266;
+	display: inline-block;
+	font-size: inherit;
+	height: 40px;
+	line-height: 40px;
+	outline: 0;
+	padding: 0 15px;
+	transition: border-color 0.2s cubic-bezier(0.645, 0.045, 0.355, 1);
+	width: 100%;
+}
+textarea {
+	height: 60px;
+}
+.isTrue {
+	color: #0a9c11;
 }
 </style>
