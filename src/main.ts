18851a4d67f6fdf84ac7330551bcd250f5721fc4
Yoyo <maonome@gmail.com>
2020-09-18 10:47:27 +0800

    少刪除

--- src/main.ts
+++ src/main.ts
@@ -5,16 +5,11 @@ import store from './store';
 import ElementUI from 'element-ui';
 import axios from 'axios';
 import locale from 'element-ui/lib/locale/lang/zh-TW.js';
-import * as EventBus from '@/utilities/event-bus';
 import 'element-ui/lib/theme-chalk/index.css';
 import './assets/style.scss';
 
 Vue.use(ElementUI, { locale });
 Vue.config.productionTip = false;
-
-axios.get('/js/travelContent.json').then((res: any) => {
-	EventBus.getSrtokeList(res.data);
-});
 
 new Vue({
 	router,
