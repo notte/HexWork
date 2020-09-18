0e3c930c0d11ce5a2a87d9ead79ff12d6d7d9f6b
Yoyo <maonome@gmail.com>
2020-09-18 10:48:39 +0800

    少刪除

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,6 @@ import App from './App.vue';
 import router from './router';
 import store from './store';
 import ElementUI from 'element-ui';
-import axios from 'axios';
 import locale from 'element-ui/lib/locale/lang/zh-TW.js';
 import 'element-ui/lib/theme-chalk/index.css';
 import './assets/style.scss';
